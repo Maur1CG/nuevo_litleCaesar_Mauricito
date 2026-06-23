8b1927c97fe39032992ad33703566574b5800940
Maur1CG <20240746@ricaldone.edu.sv>
2026-06-23 08:21:07 -0600

    db uri terminado

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import { config } from "../backend/config.js";
 
-mongoose.connect("mongodb://localhost:27017/littleCaesarDB")
+mongoose.connect(config.db.uri);
 
 //Comprobar que todo funciona
 
